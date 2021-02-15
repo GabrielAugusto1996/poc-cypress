7f55e3b80327719d9a4f269ce5e8be4c80fc41cc
Gabriel Augusto <biga00145@gmail.com>
2021-02-15 19:35:12 -0300

    Campos de Texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,7 +1,7 @@
 /// <reference types = "cypress" />
 
 describe('Work with basic elements', () => {
-    
+
     //Executa antes de todos os testes
     before(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
@@ -38,5 +38,24 @@ describe('Work with basic elements', () => {
 
         cy.get('#resultado')
             .should('have.not.text', 'Voltou!')
+    })
+
+    it.only('Text Fields', () => {
+        cy.get('#formNome')
+            .type('Cypress Text')
+            .should('have.value', 'Cypress Text')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 100 })
+            .should('have.value', 'acerto')
     })
 })
