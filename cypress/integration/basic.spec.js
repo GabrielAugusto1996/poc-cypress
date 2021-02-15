7e350a1569bcbd7f148d8cbc1f434081fddf9da8
Gabriel Augusto <biga00145@gmail.com>
2021-02-15 18:11:50 -0300

    Acessar uma página - basic.spec.js

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress" />
+
+describe('Cypress basics', () => {
+    it ('Should visit a page and assert title', () => {
+       cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+       cy.title().should('be.equal', 'Campo de Treinamento')
+       cy.title().should('contain', 'Campo')
+
+
+       cy.title()
+        .should('be.equal', 'Campo de Treinamento')
+        .and('contain', 'Campo')
+
+
+        //TODO: Desafio Cypress:
+        //TODO: Imprimir o log no console
+        //TODO: Escrever o log em um campo de texto
+    })
+})
