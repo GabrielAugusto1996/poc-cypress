a67da8c087cb30ea745e939947de86e8f594c809
Gabriel Augusto <biga00145@gmail.com>
2021-02-16 23:04:39 -0300

    Entendendo a demora do Cypress

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,25 @@
+/// <reference types = "cypress" />
+
+describe('Esperas...', () => {
+    //Executa antes de todos os testes
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+    })
+
+    //Executa antes de cada um dos testes
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Deve aguardar elemento estar disponível', () => {
+        cy.get('#novoCampo')
+            .should('not.exist')
+
+        cy.get('#buttonDelay')
+            .click()
+
+        cy.get('#novoCampo')
+            .type('funciona')
+    })
+})
