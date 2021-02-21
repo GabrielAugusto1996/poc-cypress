b2f6380445d110f9a078689cb2706b93b3280339
Gabriel Augusto <biga00145@gmail.com>
2021-02-21 20:05:20 -0300

    Popup via links

--- cypress/integration/popup.spec.js
+++ cypress/integration/popup.spec.js
@@ -12,7 +12,7 @@ describe('Work with Popup', () => {
         cy.get('#otherButton').click()
     })
 
-    it.only('Deve verificar se o popup foi invocado', () => {
+    it('Deve verificar se o popup foi invocado', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
         cy.window().then(win => {
@@ -22,6 +22,37 @@ describe('Work with Popup', () => {
         cy.get('#buttonPopUp').click()
 
         cy.get('@winOpen').should('be.called')
+    })
+
+    describe.only('With links...', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should access popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                
+                cy.visit(href)
+            })
+
+            cy.get('#tfield').type('funciona')
+                .should('have.value', 'funciona')
+        })
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+
+            cy.get('#tfield').type('funciona')
+        })
     })
 
 })
