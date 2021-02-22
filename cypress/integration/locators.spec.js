a35882744a062d13e0bdf25ff9d41aa847d3b9fe
Gabriel Augusto <biga00145@gmail.com>
2021-02-22 19:42:56 -0300

    Ordem de busca no Cypress

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,20 @@
+/// <reference types = "cypress" />
+
+describe('Work with basic elements', () => {
+
+    //Executa antes de todos os testes
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+    })
+
+    //Executa antes de cada um dos testes
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('', () => {
+        
+    })
+
+})
