6a212077dde7f1e53a9555960b5d5606c067873b
Gabriel Augusto <biga00145@gmail.com>
2021-03-26 16:54:00 -0300

    Criando commandos de conta

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,6 +1,7 @@
 /// <reference types = "cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandsConta'
 
 describe('Should test at a functional level', () => {
 
@@ -11,16 +12,13 @@ describe('Should test at a functional level', () => {
     })
 
     it('Should create an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Conta inserida')
     })
 
     it('Should update an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Conta atualizada')
