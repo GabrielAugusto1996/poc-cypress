6e41f05c9a8988c238f784a32f1e173a63425767
Gabriel Augusto <biga00145@gmail.com>
2021-03-26 16:39:54 -0300

    Organizando Locators

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,46 +1,31 @@
 /// <reference types = "cypress" />
 
+import loc from '../../support/locators'
+
 describe('Should test at a functional level', () => {
 
     //Executa antes de todos os testes
     before(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get('.input-group > .form-control')
-            .type('biga00145@gmail.com')
-
-        cy.get(':nth-child(2) > .form-control')
-            .type('Zelda-123')
-
-        cy.get('.btn').click()
-
-        cy.get('.toast-message')
-            .should('exist')
-            .and('contain', 'Bem vindo,')
+        cy.get(loc.LOGIN.USER).type('biga00145@gmail.com')
+        cy.get(loc.LOGIN.PASSWORD).type('Zelda-123')
+        cy.get(loc.LOGIN.BTN_LOGIN).click()
+        cy.get(loc.MESSAGE).should('exist').and('contain', 'Bem vindo,')
     })
 
     it('Should create an account', () => {
-        cy.get('[data-test=menu-settings] > .fas').click()
-        cy.get('[href="/contas"]').click()
-
-        cy.get('[data-test=nome]').type('Conta de teste')
-        cy.get('.btn').click()
-
-        cy.get('.toast-message')
-            .should('exist')
-            .and('contain', 'Conta inserida')
+        cy.get(loc.MENU.SETTINGS).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.get(loc.CONTAS.NOME).type('Conta de teste')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('exist').and('contain', 'Conta inserida')
     })
 
     it.only('Should update an account', () => {
-        cy.get('[data-test=menu-settings] > .fas').click()
-        cy.get('[href="/contas"]').click()
-
-        cy.xpath("//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']")
-            .click()
-
-        cy.get('.btn').click()
-
-        cy.get('.toast-message')
-            .should('exist')
-            .and('contain', 'Conta atualizada')
+        cy.get(loc.MENU.SETTINGS).click()
+        cy.get(loc.MENU.CONTAS).click()
+        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('exist').and('contain', 'Conta atualizada')
     })
 })
