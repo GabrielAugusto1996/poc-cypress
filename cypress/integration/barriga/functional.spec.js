239a38ca1b8f96aa2f33c5cf9d4506151617c105
Gabriel Augusto <biga00145@gmail.com>
2021-03-26 16:47:02 -0300

    Criando Comandos

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -6,11 +6,8 @@ describe('Should test at a functional level', () => {
 
     //Executa antes de todos os testes
     before(() => {
-        cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get(loc.LOGIN.USER).type('biga00145@gmail.com')
-        cy.get(loc.LOGIN.PASSWORD).type('Zelda-123')
-        cy.get(loc.LOGIN.BTN_LOGIN).click()
-        cy.get(loc.MESSAGE).should('exist').and('contain', 'Bem vindo,')
+        cy.login('biga00145@gmail.com', 'Zelda-123')
+        cy.resetApp()
     })
 
     it('Should create an account', () => {
@@ -21,7 +18,7 @@ describe('Should test at a functional level', () => {
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Conta inserida')
     })
 
-    it.only('Should update an account', () => {
+    it('Should update an account', () => {
         cy.get(loc.MENU.SETTINGS).click()
         cy.get(loc.MENU.CONTAS).click()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
