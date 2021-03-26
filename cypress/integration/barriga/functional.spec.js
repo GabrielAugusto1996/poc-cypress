559c2cb5c7c489c515ffbe87c454a90258a16122
Gabriel Augusto <biga00145@gmail.com>
2021-03-26 18:29:36 -0300

    Gerenciamento de massas

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -21,7 +21,7 @@ describe('Should test at a functional level', () => {
     it('Should update an account', () => {
         cy.acessarMenuConta()
         cy.get(loc.CONTAS.NOME).clear()
-        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta de teste')).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
         cy.get(loc.CONTAS.NOME).type('Conta para alterar')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Conta atualizada')
@@ -29,13 +29,13 @@ describe('Should test at a functional level', () => {
 
     it('Should not create an account with same name', () => {
         cy.acessarMenuConta()
-        cy.inserirConta('Conta para alterar')
+        cy.inserirConta('Conta mesmo nome')
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Request failed with status code 400')
     })
 
     it('Should create a new movimentation', () => {
         cy.acessarMenuMovimentacao()
-        cy.inserirMovimentacao('Nova Movimentação', 15.42, 'Gabriel', true, 'Conta para alterar')
+        cy.inserirMovimentacao('Nova Movimentação', 15.42, 'Gabriel', true, 'Conta para movimentacoes')
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Movimentação inserida')
         cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
         cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Nova Movimentação', 15)).should('exist')
@@ -44,12 +44,12 @@ describe('Should test at a functional level', () => {
     it('Should get balance', () => {
         cy.get(loc.MENU.HOME).click()
         cy.wait(500)
-        cy.xpath(loc.HOME.FN_XP_ENCONTRAR_CONTA('Conta para alterar')).should('exist')
+        cy.xpath(loc.HOME.FN_XP_ENCONTRAR_CONTA('Conta para saldo')).should('exist')
     })
 
     it('Should remove a transaction', () => {
         cy.get(loc.MENU.EXTRATO).click()
-        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO_EXCLUSAO('Nova Movimentação')).click()
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO_EXCLUSAO('Movimentacao para exclusao')).click()
         cy.get(loc.MESSAGE).should('exist').and('contain', 'Movimentação removida')
     })
 })
